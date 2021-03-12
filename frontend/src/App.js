da931ff2a761fe80068297d95b15f18c69b42f09
Moishe Barnes <moishebarnes@gmail.com>
2021-03-12 08:02:53 +0000

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Score */}
+      {/* Choices */}
+      {/* Results Page */}
+      {/* Rules CTA */}
+    </div>
+  );
+}
+
+export default App;
